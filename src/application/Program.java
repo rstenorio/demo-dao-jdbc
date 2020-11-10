83e3cfd71be718a89eeba0bc6219dbe9a5fa7636
Rodrigo Tenorio <rodrigo.s.tenorio@gmail.com>
2020-11-10 14:49:59 +0100

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
+import java.util.Date;
 import java.util.Scanner;
 
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -10,7 +12,10 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 
 		Department obj = new Department(1,"Books");
-		System.out.println(obj);
+
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 
 		sc.close();
 	}
